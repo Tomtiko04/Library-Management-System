a6138f0b94264adbba60f67aa7cd93e9dc6db714
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-02-27 12:59:56 +0100

    User controller and route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const {getAllUser, deleteUser} = require('../controllers/userController') 
 
 const router = express.Router();
+
+router.get("/", getAllUser);
+router.delete("/:userId", deleteUser);
+
 
 module.exports = router
