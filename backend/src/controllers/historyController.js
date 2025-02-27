1df0b2f2b5c60467d85f3d840f915c1498e6e0ae
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-02-27 13:03:40 +0100

    History controller

---
+++ backend/src/controllers/historyController.js
@@ -0,0 +1,11 @@
+exports.getAllHistory = async (req,res) => {
+    res.status(200).json({
+        status: "success"
+    })
+}
+
+exports.getHistory = async (req, res) => {
+	res.status(200).json({
+		status: "success",
+	});
+};
