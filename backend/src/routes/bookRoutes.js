a484b3c792ec7751783bdecc8119d97431dc4736
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-03-05 16:00:14 +0100

    CRUD for Book and Bulk Upload

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -4,7 +4,18 @@ const bookController = require('../controllers/bookController');
 const router = express.Router();
 
 
-router.route("/").get(bookController.getAllBook).post(bookController.createBook);
-router.route("/:id").get(bookController.getBook).patch(bookController.updateBook).delete(bookController.deleteBook);
+router.route("/").get(bookController.getAllBooks).post(bookController.createBook);
+router.route("/:bookId").get(bookController.getBookById).patch(bookController.updateBook).delete(bookController.deleteBook);
+router.route("/bulk-import").post(bookController.bulkImportBooks);
+
+// router.post(
+// 	"/upload-csv",
+// 	auth,
+// 	authorize(["admin", "librarian"]),
+// 	upload.single("file"),
+// 	importBooksFromCSV
+// );
+
+
 
 module.exports = router;
