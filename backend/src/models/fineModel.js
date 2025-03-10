ab7b454d0b78cc7ab6812b3a53f0d1b2f0e5ce93
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-03-10 22:32:46 +0100

    Changed the Fine Schema

--- backend/src/models/fineModel.js
+++ backend/src/models/fineModel.js
@@ -1,30 +1,30 @@
 const mongoose = require("mongoose");
 
-const fineSchema = new mongoose.Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "User",
-		required: true,
+const fineSchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		book: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Book",
+			required: true,
+		},
+		amount: {
+			type: Number,
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: ["unpaid", "paid"],
+			default: "unpaid",
+		},
+		paymentDate: { type: Date },
 	},
-	book: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Book",
-		required: true,
-	},
-	amount: {
-		type: Number,
-		required: true,
-	},
-	status: {
-		type: String,
-		enum: ["pending", "paid"],
-		default: "pending",
-	},
-	dueDate: {
-		type: Date,
-		required: true
-	}
-}, { timestamps: true });
+	{ timestamps: true }
+);
 
 const Fine = mongoose.model("Fine", fineSchema);
 
