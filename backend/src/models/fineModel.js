0eb81c442e76533a436f0d287d88362746cb087e
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-03-10 23:55:42 +0100

    Fine Management

--- backend/src/models/fineModel.js
+++ backend/src/models/fineModel.js
@@ -1,31 +1,20 @@
 const mongoose = require("mongoose");
 
-const fineSchema = new mongoose.Schema(
+const FineSchema = new mongoose.Schema(
 	{
-		user: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-		book: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Book",
-			required: true,
-		},
-		amount: {
-			type: Number,
-			required: true,
-		},
-		status: {
+		user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+		book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
+		amount: { type: Number, required: true },
+		status: { type: String, enum: ["unpaid", "paid"], default: "unpaid" },
+		paymentMethod: {
 			type: String,
-			enum: ["unpaid", "paid"],
-			default: "unpaid",
+			enum: ["Flutterwave", "Manual", "Waived"],
+			default: "Flutterwave",
 		},
+		transactionId: { type: String, unique: true, sparse: true }, // Store Flutterwave transaction ID
 		paymentDate: { type: Date },
 	},
 	{ timestamps: true }
 );
 
-const Fine = mongoose.model("Fine", fineSchema);
-
-module.exports = Fine;
+module.exports = mongoose.model("Fine", FineSchema);
