8cb3a61d12338eb883f3103b22b9a6cf5a3d86ca
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-04-09 17:25:29 +0100

    History

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const authRouter = require('./src/routes/authRoutes');
-const userRouter = require('./src/routes/userRoutes'); 
-const bookRouter = require('./src/routes/bookRoutes');
-const borrowRouter = require('./src/routes/borrowRoutes');
-const fineRouter = require('./src/routes/fineRoutes');
-const notificationRouter = require('./src/routes/notificationRoutes');
-
+const authRouter = require("./src/routes/authRoutes");
+const userRouter = require("./src/routes/userRoutes");
+const bookRouter = require("./src/routes/bookRoutes");
+const borrowRouter = require("./src/routes/borrowRoutes");
+const fineRouter = require("./src/routes/fineRoutes");
+const notificationRouter = require("./src/routes/notificationRoutes");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/books', bookRouter);
-app.use('/api/v1/borrow', borrowRouter);
-app.use('/api/v1/fine', fineRouter);
-app.use('/api/v1/notification', notificationRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/books", bookRouter);
+app.use("/api/v1/borrow", borrowRouter);
+app.use("/api/v1/fine", fineRouter);
+app.use("/api/v1/notification", notificationRouter);
 
+module.exports = app;
 
-module.exports= app;
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:5173",
+// 		credentials: true,
+// 	})
+// );
+
+// // Handle preflight requests
+// app.options("*", cors());
